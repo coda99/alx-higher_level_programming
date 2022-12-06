8345576ac05d27339876022a1b1eef02e8e5e6e2
Andre <anaki11@outlook.com>
2022-12-06 11:47:22 -0800

    code

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,5 +3,5 @@
 if __name__ == "__main__":
     def print_list_integer(my_list=[]):
         for i in range(len(my_list)):
-            print("{}".format(my_list[i]))
+            print("{:d}".format(my_list[i]))
     
