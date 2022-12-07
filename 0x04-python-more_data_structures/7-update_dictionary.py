5f0a5661db411b9890f093539d7a15db78bde704
Andre <anaki11@outlook.com>
2022-12-07 12:54:09 -0800

    7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,5 +2,5 @@
 
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
-    for key, val in a_dictionary.items():
-        print("{}: {}".format(key, val))
+
+    return (a_dictionary)
