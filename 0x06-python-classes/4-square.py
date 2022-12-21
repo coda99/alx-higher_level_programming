6640f6323f84bc38e8d3b177a4666363c528bff0
Andre <anaki11@outlook.com>
2022-12-21 01:15:28 -0800

    4 try

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+"""Class Definition"""
+
+
 class Square:
     """
     Simple class Square
@@ -16,13 +19,25 @@ class Square:
     
     @property
     def size(self):
+        """
+        size getter func
+        """
+
         return self.__size
 
     @size.setter
     def size(self, value):
+        """
+        size setter func
+        """
+
         self.__size = value
 
     def area(self):
+        """"
+        area calc func
+        """
+
         try:
             if type(self.__size) == str:
                 raise TypeError("size must be integer")
